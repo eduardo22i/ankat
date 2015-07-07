d1fbd28985c78c10429c878352d5bacf529d74c5
Eduardo Irías <eduardo22i@me.com>
2015-07-06 17:24:42 -0700

    Initial Commit

---
+++ Ankat/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Ankat
+//
+//  Created by Eduardo Irías on 7/6/15.
+//  Copyright (c) 2015 Estamp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
